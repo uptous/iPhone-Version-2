908c5915aba2efd65bd5895883b890613208f177
Roshan Mishra <roshan@virtualemployee.com>
2017-03-07 16:39:06 +0530

    Remove Old Project

--- uptous/Pods/Target Support Files/RappleProgressHUD/RappleProgressHUD-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double RappleProgressHUDVersionNumber;
-FOUNDATION_EXPORT const unsigned char RappleProgressHUDVersionString[];
-
